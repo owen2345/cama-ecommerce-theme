865ac4df7e07537dfb6971ed2f14b2026cfc486a
Owen Peredo <owenperedo@gmail.com>
2016-12-15 17:01:31 -0400

    fixed product view

--- assets/js/product_manifest.js
+++ assets/js/product_manifest.js
@@ -3,8 +3,14 @@
 //= require_self
 
 $(window).load(function() {
-    $('.flexslider').flexslider({
+    var panel = $('#product_view')
+    panel.find('.flexslider').flexslider({
         animation: "slide",
-        controlNav: "thumbnails"
+        controlNav: "thumbnails",
+        slideshow: false
     });
+    panel.find('#product_variation_id').change(function(){
+        var op = $(this).children('option:selected');
+        panel.find('#product_slider .flex-control-thumbs li:eq('+$(this).children('option').index(op)+') img').click();
+    }).trigger('change');
 });
